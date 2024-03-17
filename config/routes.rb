c0e5c43a66e3790d522a2f037d6f789832abd2ae
LeandroVeiga9 <lveiga301@gmail.com>
2024-03-16 21:40:30 -0300

    disable update and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  resources :transactions
+  resources :transactions, :except => [:update, :destroy]
   devise_for :users, controllers: {sessions: :sessions}, defaults: {format: :json}, path_names: {sign_in: :login, sign_out: :logout}
   resources :users
 
